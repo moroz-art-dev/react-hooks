81ea95455ddd11ce88e829481bc22261c9d88b53
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-16 03:01:05 +0300

    Incorporating the useState Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+function App({name}) {
   return (
     <div className="App">
-      
+      <h1>Hello {name}!</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  const [status, setStatus] = useState("Not Delivered");
+  return (
+    <div className="App">
+      <h1>The package is: {status}.</h1>
+      <button onClick={() => setStatus("Delivered")}>Deliver</button>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="Artem" />
   </React.StrictMode>
 );
