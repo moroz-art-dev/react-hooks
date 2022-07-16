be4e2bbc9774efe0bde481af79fb1e4f1d8563ab
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-16 03:09:35 +0300

    Building a checkbox with useState

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import "./index.css";
 
 function App() {
   const [status, setStatus] = useState("Not Delivered");
+  const [checked, setChecked] = useState(false);
   return (
     <div className="App">
       <h1>The package is: {status}.</h1>
       <button onClick={() => setStatus("Delivered")}>Deliver</button>
+      <hr/>
+      <input
+       type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+         />
+      <p>{checked ? "checked" : "not checked"}</p>
     </div>
   );
 }
