253dd0ea32eff5be138700e808de74b729af5f1b
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-17 02:44:31 +0300

    Working with component trees

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { FaStar } from "react-icons/fa";
+
+// function App() {
+//   const [status, setStatus] = useState("Not Delivered");
+//   const [checked, setChecked] = useState(false);
+//   return (
+//     <div className="App">
+//       <h1>The package is: {status}.</h1>
+//       <button onClick={() => setStatus("Delivered")}>Deliver</button>
+//       <hr/>
+//       <input
+//        type="checkbox"
+//         value={checked}
+//         onChange={() => setChecked((checked) => !checked)}
+//          />
+//       <p>{checked ? "checked" : "not checked"}</p>
+//     </div>
+//   );
+// }
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false }) {
+  return <FaStar color={selected ? "red" : "gray"} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
+}
 
 function App() {
-  const [status, setStatus] = useState("Not Delivered");
-  const [checked, setChecked] = useState(false);
-  return (
-    <div className="App">
-      <h1>The package is: {status}.</h1>
-      <button onClick={() => setStatus("Delivered")}>Deliver</button>
-      <hr/>
-      <input
-       type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-         />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
-  );
+  return <StarRating totalStars={10} />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
