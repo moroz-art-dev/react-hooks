6f9965384ca93fb7c05007bf380ea8f1ee0d9ed4
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-19 02:14:41 +0300

    Introducing useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { FaStar } from "react-icons/fa";
@@ -20,30 +20,45 @@ import { FaStar } from "react-icons/fa";
 //     </div>
 //   );
 // }
-const createArray = (length) => [...Array(length)];
 
-function Star({ selected = false, onSelect }) {
-  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
-}
+// useState
+// const createArray = (length) => [...Array(length)];
 
-function StarRating({ totalStars = 5 }) {
-  const [selectedStarts, setSelectedStars] = useState(0);
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star
-          key={i}
-          selected={selectedStarts > i}
-          onSelect={() => setSelectedStars(i + 1)}
-        />
-      ))}
-      <p>{selectedStarts} of {totalStars}</p>
-    </>
-  );
-}
+// function Star({ selected = false, onSelect }) {
+//   return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
+// }
+
+// function StarRating({ totalStars = 5 }) {
+//   const [selectedStarts, setSelectedStars] = useState(0);
+//   return (
+//     <>
+//       {createArray(totalStars).map((n, i) => (
+//         <Star
+//           key={i}
+//           selected={selectedStarts > i}
+//           onSelect={() => setSelectedStars(i + 1)}
+//         />
+//       ))}
+//       <p>{selectedStarts} of {totalStars}</p>
+//     </>
+//   );
+// }
+
+// function App() {
+//   return <StarRating totalStars={10} />;
+// }
 
 function App() {
-  return <StarRating totalStars={10} />;
+  const [name, setName] = useState("Jan");
+  useEffect(() => {
+    document.title = `Celebrate ${name}`;
+  });
+  return (
+    <section>
+      <p>Congratulations {name}!</p>
+      <button onClick={() => setName("Will")}>Change Winner</button>
+    </section>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
