1f875b35877eaf0c0cfbe7b76f14b424ca4af9f1
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-20 03:33:02 +0300

    Working with the dependency array

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { FaStar } from "react-icons/fa";
+//import { FaStar } from "react-icons/fa";
 
 // function App() {
 //   const [status, setStatus] = useState("Not Delivered");
@@ -50,13 +50,19 @@ import { FaStar } from "react-icons/fa";
 
 function App() {
   const [name, setName] = useState("Jan");
+  const [admin, setAdmin] = useState(false);
   useEffect(() => {
-    document.title = `Celebrate ${name}`;
-  });
+    console.log = `Celebrate ${name}`;
+  }, [name]);
+  useEffect(() => {
+    console.log = `The user is: ${admin ? "admin" : "not admin"}.`;
+  }, [admin ]);
   return (
     <section>
       <p>Congratulations {name}!</p>
       <button onClick={() => setName("Will")}>Change Winner</button>
+      <p>{admin ? "logged in" : "not loggen in"}</p>
+      <button onClick={() => setAdmin(!admin)}>Log In</button>
     </section>
   );
 }
