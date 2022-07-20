b92632c03156a68c1bf3080a81a41d544d1c63e4
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-21 01:23:16 +0300

    Dispatching actions with useReducer

--- src/index.js
+++ src/index.js
@@ -10,13 +10,39 @@ import "./index.css";
 //   return <h1 onClick={() => setNumber(1)}>{number}</h1>;
 // }
 
+// function App() {
+//   const [checked, toggle] = useReducer((checked) => !checked, false);
+//   return (
+//     <div className="App">
+//       <input type="checkbox" value={checked} onChange={toggle} />
+//       {checked ? "checked" : "not checked"}
+//     </div>
+//   );
+// }
+
+const initialState = {
+  message: "Hi!",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "yell":
+      return {
+        message: `I just said ${state.message}`,
+      };
+    case "wisper":
+      return {
+        message: "excuse me",
+      };
+  }
+}
 function App() {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <input type="checkbox" value={checked} onChange={toggle} />
-      {checked ? "checked" : "not checked"}
-    </div>
+    <>
+      <p>Message: {state.message}</p>
+      <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
+      <button onClick={() => dispatch({ type: "wisper" })}>wisper</button>
+    </>
   );
 }
 
