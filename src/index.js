af7765d87ff97910d0c6c1da6f4b776b03b62ae5
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-22 17:35:00 +0300

    Managing form inputs with useRef

--- src/index.js
+++ src/index.js
@@ -1,49 +1,9 @@
-import React, { useReducer } from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-// function App() {
-//   const [number, setNumber] = useReducer(
-//     (number, newNumber) => number + newNumber,
-//     0
-//   );
-//   return <h1 onClick={() => setNumber(1)}>{number}</h1>;
-// }
-
-// function App() {
-//   const [checked, toggle] = useReducer((checked) => !checked, false);
-//   return (
-//     <div className="App">
-//       <input type="checkbox" value={checked} onChange={toggle} />
-//       {checked ? "checked" : "not checked"}
-//     </div>
-//   );
-// }
-
-const initialState = {
-  message: "Hi!",
-};
-function reducer(state, action) {
-  switch (action.type) {
-    case "yell":
-      return {
-        message: `I just said ${state.message}`,
-      };
-    case "wisper":
-      return {
-        message: "excuse me",
-      };
-  }
-}
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <>
-      <p>Message: {state.message}</p>
-      <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
-      <button onClick={() => dispatch({ type: "wisper" })}>wisper</button>
-    </>
-  );
+  
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/tempHooks/useRef.js
@@ -0,0 +1,32 @@
+import React, { useRef } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function App() {
+  const sound = useRef();
+  const color = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+    alert(`${soundVal} sounds like ${colorVal}`);
+    sound.current.value = "";
+    color.current.value = "";
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <input ref={sound} type="text" placeholder="Sound..." />
+      <input ref={color} type="color" />
+      <button>ADD</button>
+    </form>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
