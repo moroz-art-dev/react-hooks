3e69252b4b02146bb7fdbbd03c421ccb22f401b7
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-22 23:27:28 +0300

    Data fetching with a Fetch Hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
+function App() {}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <TreesContext.Provider value={{ trees }}>
-    <App />
-  </TreesContext.Provider>
-);
+root.render(<App />);
